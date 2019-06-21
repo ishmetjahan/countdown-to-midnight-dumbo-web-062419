9014024167e010128c3399b617bc699e8fc02dfa
Ishmet <ishmetjahan@yahoo.com>
2019-06-21 19:03:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,8 +4,6 @@ def countdown (number)
   while number>0  
     puts "#{number} SECOND(S)!"
     number -=1 
-    time = Time.now
- sleep 2.seconds until Time.now > time + 10.seconds # breaks when true
   end
   return "HAPPY NEW YEAR!"
 end
@@ -13,9 +11,9 @@ end
 # 
 def countdown_with_sleep(num)
   while num>0 
+  sleep 1
   puts "#{num} SECOND(S)!"
     num -=1 
-    num(1)
   end
   return "HAPPY NEW YEAR!"
 end
